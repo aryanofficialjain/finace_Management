21485e325aaaf9ad4a537aa5a1b2fdd34e47994e
Aryan Jain <aryanofficialjain@gmail.com>
2026-01-19 20:12:54 +0530

    latest chnaged pushed

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,3 @@
 // export const API_URL = "https://wallet-api-cxqp.onrender.com/api";
-export const API_URL = "http://localhost:3000/api";
+// export const API_URL = "http://localhost:3000/api";
+export const API_URL = "https://finace-management.onrender.com/api";
